d7ba731f550dd8326f36b81b84f43a498e4943ae
Santiago <santiago.guti17correa@gmail.com>
2023-02-15 08:27:42 -0500

    Commit 1

---
+++ punto_1.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def accelerate(self):
+        print("Accelerating.")
+
+    def brake(self):
+        print("Braking.")
