3be2d83e6aa0ed092e133c7ceb58626b5f4dd764
Santiago <santiago.guti17correa@gmail.com>
2023-02-15 09:13:32 -0500

    Commit 10

--- punto_2.py
+++ punto_2.py
@@ -1,4 +1,4 @@
-class Punto:
+class punto:
     def __init__(self, x, y):
         self.x = x
         self.y = y
--- punto_3.py
+++ punto_3.py
@@ -1,7 +1,7 @@
 import math
 
 
-class Punto:
+class punto:
     def __init__(self, x, y):
         self.x = x
         self.y = y
