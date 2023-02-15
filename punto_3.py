5cbbeaa1f02ece03ce1155c2f31d8069f4da26a9
Santiago <santiago.guti17correa@gmail.com>
2023-02-15 08:49:29 -0500

    Commit 6

---
+++ punto_3.py
@@ -0,0 +1,20 @@
+import math
+
+
+class Punto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def mostrar(self):
+        print(f"Coordenadas del punto: ({self.x}, {self.y})")
+
+    def mover(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def calcular_distancia(self, otro_punto):
+        dx = self.x - otro_punto.x
+        dy = self.y - otro_punto.y
+        distancia = math.sqrt(dx ** 2 + dy ** 2)
+        return distancia
