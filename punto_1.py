607ebe1f26fe996524376bed95c94c3610c69733
Santiago <santiago.guti17correa@gmail.com>
2023-02-15 08:53:41 -0500

    Commit 7

--- punto_1.py
+++ punto_1.py
@@ -1,10 +1,4 @@
-class Car:
-    def __init__(self, max_speed, mileage):
-        self.max_speed = max_speed
-        self.mileage = mileage
-
-    def accelerate(self):
-        print("Accelerating.")
-
-    def brake(self):
-        print("Braking.")
+class vehiculo:
+    def __init__(self, velocidad_maxima, kilometraje):
+        self.velocidad_maxima = velocidad_maxima
+        self.kilometraje = kilometraje
--- punto_4.py
+++ punto_4.py
@@ -1,19 +1,19 @@
-class Rectangle:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
+class rectangulo:
+    def __init__(self, esquina1, esquina2):
+        self.esquina1 = esquina1
+        self.esquina2 = esquina2
 
-    def perimeter(self):
-        width = abs(self.point2.x - self.point1.x)
-        height = abs(self.point2.y - self.point1.y)
-        return 2 * (width + height)
+    def calcular_perimetro(self):
+        base = abs(self.esquina1.x - self.esquina2.x)
+        altura = abs(self.esquina1.y - self.esquina2.y)
+        return 2 * (base + altura)
 
-    def area(self):
-        width = abs(self.point2.x - self.point1.x)
-        height = abs(self.point2.y - self.point1.y)
-        return width * height
+    def calcular_area(self):
+        base = abs(self.esquina1.x - self.esquina2.x)
+        altura = abs(self.esquina1.y - self.esquina2.y)
+        return base * altura
 
-    def is_square(self):
-        width = abs(self.point2.x - self.point1.x)
-        height = abs(self.point2.y - self.point1.y)
-        return width == height
+    def es_cuadrado(self):
+        base = abs(self.esquina1.x - self.esquina2.x)
+        altura = abs(self.esquina1.y - self.esquina2.y)
+        return base == altura
